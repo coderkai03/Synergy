77dae2090aafc2305995b6a93560c45d5b802e6e
Rian Corcino <riancorci@gmail.com>
2025-01-05 00:11:59 -0800

    removed var

--- components/team-form.tsx
+++ components/team-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useUser } from "@clerk/nextjs";
 import { toast } from "react-hot-toast";
 import { Team } from "@/types/Teams";
